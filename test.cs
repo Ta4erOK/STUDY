d0861f30b8867fc89172114cf1c5e5102438ad1d
Ta4erOK <dagienko79@gmail.com>
2026-01-15 14:45:24 +0700

    calculator

--- test.cs
+++
@@ -1,4 +0,0 @@
-int a = 1;
-int b = 1;
-
-Console.WriteLine(a+b);
--- testy/Program.cs
+++ testy/Program.cs
@@ -4,5 +4,36 @@ int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите Второе число: ");
 int b = Convert.ToInt32(Console.ReadLine());
 
+int c = 0;
 
-Console.WriteLine($"Итого: {a} + {b} = {a + b}");
+Console.Write("Выберите операцию:\n1-сложение \n2-вычитание \n3-умножение \n4-деление \n \n");
+int o = Convert.ToInt32(Console.ReadLine());
+
+if (o == 1)
+{
+    Console.WriteLine($"Сложение");
+    c = a + b;
+}
+else if (o == 2)
+{
+    Console.WriteLine($"Вычитание");
+    c = a - b;
+}
+else if (o == 3)
+{
+    Console.WriteLine($"Умножение");
+    c = a * b;
+}
+else if (o == 4)
+{
+    Console.WriteLine($"Деление");
+    c = a / b;
+}
+else 
+{
+    Console.WriteLine($"Ошибка");
+}
+
+Console.WriteLine($"\nИтого:{c}");
+//Console.WriteLine($"Итого: {a} + {b} = {a + b}");
+
